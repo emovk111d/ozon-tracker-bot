f3611a5ed13462e643d3f15e94e4357f847c30a8
emovk111d <feda.feda005@gmail.com>
2026-02-23 14:49:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -e
-gunicorn -b 0.0.0.0:$PORT main:app
+
+# веб для Render (порт)
+gunicorn -b 0.0.0.0:$PORT main:app &
+
+# телеграм polling как главный процесс
+python main.py
